0e8e254cc4c34d7b7ebcc8ecba604c664d29b53a
Matt Garrison <matt@iconoclastlabs.com>
2014-01-03 10:45:25 -0600

    updating emacs configs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -115,3 +115,22 @@
   (require 'sr-speedbar)
   ;;(speedbar 1)
   (setq speedbar-use-images nil)
+
+  ;; disable the toolbar
+  (tool-bar-mode -1)
+  ;; Add movement keybindings
+  (global-set-key [M-left] 'windmove-left)          ; move to left windnow
+  (global-set-key [M-right] 'windmove-right)        ; move to right window
+  (global-set-key [M-up] 'windmove-up)              ; move to upper window
+  (global-set-key [M-down] 'windmove-down)          ; move to lower window
+
+  ;; Change backup behavior
+  (setq
+     backup-by-copying t      ; don't clobber symlinks
+     backup-directory-alist
+      '(("." . "~/.emacs-saves"))    ; don't litter my fs tree
+     delete-old-versions t
+     kept-new-versions 6
+     kept-old-versions 2
+     version-control t)       ; use versioned backups
+
